57a9507a4464f2d696eb8fd3592a5d698fcdcdb1
Bass Jobsen <bass@w3masters.nl>
2013-09-17 23:15:18 +0200

    Update core.php
    Fix for issue #37 by @quokka

--- library/core.php
+++ library/core.php
@@ -277,7 +277,7 @@ function skematik_logo() {
 	{
 	return "<a id='logo-link-container' href='".home_url()."' title='".esc_attr( get_bloginfo( 'name', 'display' ) )."'><img class='site-logo' src='$custom_logo' alt='".esc_attr( get_bloginfo( 'name', 'display' ) )."' /></a>"; 
 	} 
-	return '<a class="navbar-brand" href="'. home_url() . '">' . bloginfo('name') . '</a>';
+	return '<a class="navbar-brand" href="'. home_url() . '">' . get_bloginfo('name') . '</a>';
 }
 
 
