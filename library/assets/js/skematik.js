359bde9964d855c671426ec5b0907989963d6b91
Bass Jobsen <bass@w3masters.nl>
2013-10-22 20:33:11 +0200

    fix issue #40

--- library/assets/js/skematik.js
+++ library/assets/js/skematik.js
@@ -2,3 +2,6 @@
 jQuery(document).ready(function($) {
 	$('.gallery .gallery-icon a').attr('rel', 'lightbox[gallery]');
 });
+
+/* make clickable */
+jQuery('.nav a.dropdown-toggle').on('click', function (e) { if(jQuery(this).attr('href') !== "#" ) { location.href = jQuery(this).attr('href');} return true; })
