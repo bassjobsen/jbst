e4ff216e2d192bd53b53899d87c26cc870e9f4af
Bass Jobsen <bass@w3masters.nl>
2013-10-31 21:03:06 +0100

    fixes for #35 and #53

--- searchform.php
+++ searchform.php
@@ -7,7 +7,9 @@
  */
 ?>
 	<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-		<label for="s" class="assistive-text"><?php _e( 'Search', 'jamedo-bootstrap-start-theme' ); ?></label>
-		<input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'jamedo-bootstrap-start-theme' ); ?>" />
-		<input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'jamedo-bootstrap-start-theme' ); ?>" />
+		<div class="form-group">
+		<label for="s" class="sr-only"><?php _e( 'Search', 'jamedo-bootstrap-start-theme' ); ?></label>
+		<input type="text" class="form-control" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', 'jamedo-bootstrap-start-theme' ); ?>" />
+		</div>
+		<input type="submit" class="btn <?php skematik_button_class();?>" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', 'jamedo-bootstrap-start-theme' ); ?>" />
 	</form>
