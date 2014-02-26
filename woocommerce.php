7811d7ea65540f5ca222abd9a21d5b979570434a
Bass Jobsen <bass@w3masters.nl>
2014-02-26 14:03:48 +0100

    fixes for issue #112

--- woocommerce.php
+++ woocommerce.php
@@ -8,5 +8,11 @@
  * @since 2.0.6
  */
 get_header(); 
-woocommerce_content();
+if ( is_singular( 'product' ) ) {
+     woocommerce_content();
+  }else{
+   //For ANY product archive.
+   //Product taxonomy, product search or /shop landing
+    woocommerce_get_template( 'archive-product.php' );
+  }
 get_footer();
